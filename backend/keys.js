a597ffa3678b5896f44d2dc26f8eadc40ae39677
Toshika Raikwar <raikwartoshika@gmail.com>
2023-06-28 12:03:05 +0530

    Initial

---
+++ backend/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://raikwartoshika:9w50ybkB4WQbCtbE@cluster0.zqsomys.mongodb.net/?retryWrites=true&w=majority",
+    Jwt_secret: "9w50ybkB4WQbCtbE"
+
+};
