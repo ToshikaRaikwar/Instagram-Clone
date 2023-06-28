eb665804042147e8e6bd5b6a1613675a0ff67b2e
Toshika Raikwar <95243378+ToshikaRaikwar@users.noreply.github.com>
2023-06-28 13:38:42 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import React, { createContext, useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
@@ -6,35 +5,36 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
-import Profie from "./components/Profie";
+import Profile from "./components/Profile";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Createpost from "./components/Createpost";
+import CreatePost from "./components/CreatePost";
 import { LoginContext } from "./context/LoginContext";
 import Modal from "./components/Modal";
-import UserProfie from "./components/UserProfile";
-import MyFolliwngPost from "./components/MyFollowingPost";
+import UserProfile from "./components/UserProfile";
+import MyFollowingPost from "./components/MyFollowingPost";
 
 function App() {
   const [userLogin, setUserLogin] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <BrowserRouter>
       <div className="App">
         <LoginContext.Provider value={{ setUserLogin, setModalOpen }}>
           <Navbar login={userLogin} />
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-            <Route path="/signin" element={<SignIn />}></Route>
-            <Route exact path="/profile" element={<Profie />}></Route>
-            <Route path="/createPost" element={<Createpost />}></Route>
-            <Route path="/profile/:userid" element={<UserProfie />}></Route>
-            <Route path="/followingpost" element={<MyFolliwngPost />}></Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/createPost" element={<CreatePost />} />
+            <Route path="/profile/:userid" element={<UserProfile />} />
+            <Route path="/followingpost" element={<MyFollowingPost />} />
           </Routes>
           <ToastContainer theme="dark" />
 
-          {modalOpen && <Modal setModalOpen={setModalOpen}></Modal>}
+          {modalOpen && <Modal setModalOpen={setModalOpen} />}
         </LoginContext.Provider>
       </div>
     </BrowserRouter>
